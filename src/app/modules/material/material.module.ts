316eb284de7cbea78799d75f75f5ab9ae27de332
sollenne <rich@sollenne.com>
2021-07-27 00:53:09 -0500

    update to angular material to v10.x

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,6 +1,6 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MATERIAL_SANITY_CHECKS } from '@angular/material';
+import { MATERIAL_SANITY_CHECKS } from '@angular/material/core';
 
 /** MATERIAL COMPONENTS */
 import { MatGridListModule } from '@angular/material/grid-list';
