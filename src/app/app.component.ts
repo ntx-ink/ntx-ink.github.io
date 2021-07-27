963a057d4f866c046ebf623397b2ee4bb3714ab6
sollenne <rich@sollenne.com>
2021-07-26 23:37:20 -0500

    change build directory to docs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ntx';
+export class AppComponent implements OnInit {
+  public breakpoint: number;
+
+  ngOnInit() {
+    this.breakpoint = (window.innerWidth <= 600) ? 4 : 8;
+  }
 }
---
+++ src/app/modules/material/material.module.ts
@@ -0,0 +1,29 @@
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MATERIAL_SANITY_CHECKS } from '@angular/material';
+
+/** MATERIAL COMPONENTS */
+import { MatGridListModule } from '@angular/material/grid-list';
+
+const materialModules = [
+  MatGridListModule
+];
+
+@NgModule({
+  exports: materialModules,
+  imports: [
+    CommonModule
+  ],
+  providers: [
+    {
+      provide: MATERIAL_SANITY_CHECKS,
+      useValue: false,
+    },
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA,
+  ]
+})
+
+export class MaterialModule { }
